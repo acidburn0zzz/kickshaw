c4f6eb904b610fd7ed4e90c5f159120ebc1ccb99
natemaia <natemaia10@gmail.com>
2017-08-03 16:41:49 -0700

    update install script

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 cd source/
 make
+clear
+echo "#################################################################
+#####       The following requires root privileges        ########
+##################################################################"
 sudo make install
 
 exit 0
